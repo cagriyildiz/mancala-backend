50b24f7e2f5fed85f8aae6f715bd6f7c630e5d81
cagriyildiz <cagriyild@gmail.com>
2021-12-11 16:40:38 +0300

    Create DTO object for game entity

---
+++ src/main/java/com/bol/mancala/web/model/GameDto.java
@@ -0,0 +1,49 @@
+package com.bol.mancala.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static com.bol.mancala.domain.Game.MIN_INITIAL_STONE_COUNT;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GameDto {
+
+  @Null
+  private UUID id;
+
+  @Null
+  private Integer version;
+
+  @Null
+  private OffsetDateTime createdDate;
+
+  @Null
+  private OffsetDateTime lastModifiedDate;
+
+  @NotNull
+  @Positive
+  private Long firstPlayerId;
+
+  @NotNull
+  @Positive
+  private Long secondPlayerId;
+
+  @Min(MIN_INITIAL_STONE_COUNT)
+  private Integer initialStoneCount;
+
+  @Positive
+  private Long activePlayerId;
+
+}
