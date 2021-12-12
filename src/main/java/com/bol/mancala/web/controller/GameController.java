b4b812dba14467b8fe2c8ece80863653313aaf5e
cagriyildiz <cagriyild@gmail.com>
2021-12-12 14:25:14 +0300

    Expose an endpoint to play the game

--- src/main/java/com/bol/mancala/web/controller/GameController.java
+++ src/main/java/com/bol/mancala/web/controller/GameController.java
@@ -8,6 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.UUID;
+
+import static com.bol.mancala.domain.Board.BOARD_SIZE_X;
+
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/game/")
 @RestController
@@ -19,6 +26,13 @@ public class GameController {
   public ResponseEntity<GameDto> createNewGame(@RequestBody @Validated GameDto gameDto) {
     GameDto newGame = gameService.createNewGame(gameDto);
     return new ResponseEntity<>(newGame, HttpStatus.CREATED);
+  }
+
+  @GetMapping(value = "play/{gameId}")
+  public ResponseEntity<GameDto> playGame(@PathVariable UUID gameId,
+                                          // only small pits are accepted as a parameter
+                                          @RequestParam(value = "pit") @PositiveOrZero @Max(BOARD_SIZE_X - 2) int pit) {
+    return new ResponseEntity<>(HttpStatus.OK);
   }
 
 }
--- src/test/java/com/bol/mancala/web/controller/GameControllerTest.java
+++ src/test/java/com/bol/mancala/web/controller/GameControllerTest.java
@@ -11,8 +11,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static com.bol.mancala.domain.Board.BOARD_SIZE_X;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,6 +26,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class GameControllerTest {
 
   protected static final String ENDPOINT_CREATE = "/api/v1/game/create/";
+  protected static final String ENDPOINT_PLAY = "/api/v1/game/play/";
+
+  protected static final int PIT_INDEX_BIG = BOARD_SIZE_X - 1;
+  protected static final int PIT_INDEX_VALID = ThreadLocalRandom.current().nextInt(0, PIT_INDEX_BIG);
+
+  protected static final String PARAM_PIT = "pit";
+  protected static final String PARAM_PIT_VAL = String.valueOf(PIT_INDEX_VALID);
+  protected static final String PARAM_PIT_INVALID_MIN_VAL = "-1"; // any negative value can be accepted as invalid input
+  protected static final String PARAM_PIT_INVALID_MAX_VAL = String.valueOf(PIT_INDEX_BIG);
 
   protected static final long FIRST_PLAYER_ID = 1L;
   protected static final long SECOND_PLAYER_ID = 2L;
@@ -50,6 +64,45 @@ class GameControllerTest {
             .contentType(MediaType.APPLICATION_JSON)
             .content(gameDtoJson))
         .andExpect(status().isCreated());
+  }
+
+  @DisplayName("Play Game")
+  @Test
+  void playGame() throws Exception {
+    mockMvc.perform(
+            get(ENDPOINT_PLAY + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .param(PARAM_PIT, PARAM_PIT_VAL))
+        .andExpect(status().isOk());
+  }
+
+  @DisplayName("Play Game Without Providing a Pit")
+  @Test
+  void playGameWithoutProvidingPit() throws Exception {
+    mockMvc.perform(
+            get(ENDPOINT_PLAY + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("Play Game With Invalid Min Pit")
+  @Test
+  void playGameWithInvalidMinPit() throws Exception {
+    mockMvc.perform(
+            get(ENDPOINT_PLAY + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .param(PARAM_PIT, PARAM_PIT_INVALID_MIN_VAL))
+        .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("Play Game With Invalid Max Pit")
+  @Test
+  void playGameWithInvalidMaxPit() throws Exception {
+    mockMvc.perform(
+            get(ENDPOINT_PLAY + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .param(PARAM_PIT, PARAM_PIT_INVALID_MAX_VAL))
+        .andExpect(status().isBadRequest());
   }
 
 }
