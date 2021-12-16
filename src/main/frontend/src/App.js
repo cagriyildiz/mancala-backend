94b3b666654cb080d3e89942d6635e3f63f1d756
cagriyildiz <cagriyild@gmail.com>
2021-12-16 19:49:48 +0300

    Create a game session

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,13 +1,29 @@
 import {useState} from "react";
+import axios from "axios";
 import './App.css';
 import Board from "./component/Board/Board";
 
 function App() {
 
+  const createGameEndpoint = 'http://localhost:8080/api/v1/game/create/';
+
   const [playing, setPlaying] = useState(false);
-  const [gameState] = useState([[6, 6, 6, 6, 6, 6, 0], [6, 6, 6, 6, 6, 6, 0]]);
+  const [gameState, setGameState] = useState([[6, 6, 6, 6, 6, 6, 0], [6, 6, 6, 6, 6, 6, 0]]);
+
+  const getDefaultRequestPayload = () => {
+    return {
+      "firstPlayerId": "123",
+      "secondPlayerId": "456"
+    };
+  };
 
   const startGame = () => {
+    axios.post(createGameEndpoint, getDefaultRequestPayload())
+      .then(response => {
+        setGameState(response.data.board.pits);
+      }).catch(error => {
+      console.error(error);
+    });
     setPlaying(true);
   };
 
