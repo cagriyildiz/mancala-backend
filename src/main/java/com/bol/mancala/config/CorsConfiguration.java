4c568c028775cf963abd14de77c3a7b4b7ddafc8
cagriyildiz <cagriyild@gmail.com>
2021-12-18 18:37:01 +0300

    Enable CORS

---
+++ src/main/java/com/bol/mancala/config/CorsConfiguration.java
@@ -0,0 +1,35 @@
+package com.bol.mancala.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "bol.com.mancala.cors")
+@Configuration
+@Component
+public class CorsConfiguration {
+
+  private String[] allowedOrigins;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name());
+      }
+    };
+  }
+
+}
