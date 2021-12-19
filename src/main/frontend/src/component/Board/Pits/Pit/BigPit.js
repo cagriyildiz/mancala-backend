81413a5b0f67dab3f18827a4f907c94d358822f2
cagriyildiz <cagriyild@gmail.com>
2021-12-19 15:27:23 +0300

    Remove unnecessary wrapper tag

--- src/main/frontend/src/component/Board/Pits/Pit/BigPit.js
+++ src/main/frontend/src/component/Board/Pits/Pit/BigPit.js
@@ -3,7 +3,7 @@ import React from "react";
 const BigPit = (props) => {
   return (
     <div className={["big-pit", `player-${props.player}`].join(" ")}>
-      <p>{props.store > 0 ? props.store : null}</p>
+      {props.store > 0 ? props.store : null}
     </div>
   );
 };
